1d2561f00711fe5bf9242c11164878de094249af
dgoldsb <dgoldsb@live.nl>
2022-08-22 21:49:27 +0200

    Mine advent coins

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,24 @@ fn count_char(string: String, target: char) -> PyResult<usize> {
     Ok(string.chars().filter(|&c| c == target).count())
 }
 
+#[pyfunction]
+fn mine_advent_coin(string: String, start: String) -> PyResult<usize> {
+    let mut value: usize = 0;
+
+    loop {
+        let mut input = string.clone();
+        input.push_str(&value.to_string());
+        let digest = format!("{:x}", md5::compute(input.as_bytes()));
+        if digest.starts_with(&start) {
+            return Ok(value);
+        }
+        value += 1;
+    }
+}
+
 #[pymodule]
 fn aoc_py03(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(count_char, m)?)?;
+    m.add_function(wrap_pyfunction!(mine_advent_coin, m)?)?;
     Ok(())
 }
